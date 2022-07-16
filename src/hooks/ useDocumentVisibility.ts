37be416df9f34737ee05ee298580f8466ef721fb
Ivan Koigerov <heartsf0xs@gmail.com>
2022-07-16 14:53:43 +0300

    feat(hook): create hook document visible

---
+++ src/hooks/ useDocumentVisibility.ts
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react';
+
+export const  useDocumentVisibility = () => {
+    const [visible, setVisible] = useState<boolean>(!document.hidden);
+    const [count, setCount] = useState<number>(0)
+
+    function onVisibleChange (func: (isVisible: boolean) => void) {
+        func(visible);
+    }
+
+    useEffect(() => {
+        document.addEventListener("visibilitychange", () => setVisible(!document.hidden));
+
+        return () => {
+            document.removeEventListener("visibilitychange", () => setVisible(!document.hidden));
+
+            if(!visible){
+                setCount(count+1);
+            }
+        }
+    }, [visible]);
+
+    return {visible, count, onVisibleChange};
+}
