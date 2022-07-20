9c144c0788bd1fd2ff74172e7a2d8a7ecb1f66df
Ivan Koigerov <heartsf0xs@gmail.com>
2022-07-20 19:04:49 +0300

    fix(MediaQuery): change queryCreate

--- src/react-responsives/MediaQuery.tsx
+++ src/react-responsives/MediaQuery.tsx
@@ -1,26 +1,24 @@
-import { useCallback } from 'react';
 import useMediaQuery from './useMediaQuery';
 import { MediaQueryProps } from './MediaQuery.types';
 
+const queryCreate = (props: object): string => {
+  const matchesName = (name: string) => {
+    return name.replace(/[A-Z]/g, (name) => ('-' + name.toLowerCase()));
+  };
+  return Object.entries(props).map(([key, value]) => {
+    switch (key) {
+      case 'orientation':
+        return `(${key}: ${value})`;
+      case 'maxResolution':
+      case 'minResolution':
+        return typeof value === 'number' ? `(${matchesName(key)}: ${value}dppx)` : `(${matchesName(key)}: ${value})`;
+      default:
+        return typeof value === 'number' ? `(${matchesName(key)}: ${value}px)` : `(${matchesName(key)}: ${value})`;
+    }
+  }).join(' and ');
+};
+
 const MediaQuery = ({ children, ...props }: MediaQueryProps) => {
-
-  const queryCreate = useCallback((props: object): string => {
-    const matchesName = (name: string) => {
-      return name.replace(/[A-Z]/g, (name) => ('-' + name.toLowerCase()));
-    };
-    return Object.entries(props).map(([key, value]) => {
-      switch (key) {
-        case 'orientation':
-          return `(${key}: ${value})`;
-        case 'maxResolution':
-        case 'minResolution':
-          return typeof value === 'number' ? `(${matchesName(key)}: ${value}dppx)` : `(${matchesName(key)}: ${value})`;
-        default:
-          return typeof value === 'number' ? `(${matchesName(key)}: ${value}px)` : `(${matchesName(key)}: ${value})`;
-      }
-    }).join(' and ');
-  }, []);
-
   const matches = useMediaQuery(queryCreate(props));
 
   return typeof children === 'function' ? children(matches) : matches ? children : null;
